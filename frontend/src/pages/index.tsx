8300212bed0c698bb5d77de3d0d9bbe15b4df9fa
gabrielglp <gabriel_pg08@hotmail.com>
2024-07-05 18:30:01 -0300

    feat: data sending settings

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useContext, FormEvent, useState } from "react";
+
 import Head from "next/head";
 
 import LogoImg from '../../public/logo.svg'
@@ -8,7 +10,27 @@ import { Button } from "../components/ui/Button";
 
 import Link from "next/link";
 
+import { AuthContext } from "../contexts/AuthContext";
+
 export default function Home() {
+  const {signIn} = useContext(AuthContext)
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(e: FormEvent) {
+    e.preventDefault();
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
       <Head>
@@ -21,10 +43,10 @@ export default function Home() {
         />
 
         <div className="mt-8 w-full md:w-[600px] flex items-center justify-center flex-col py-8 px-8">
-          <form className="w-11/12 flex flex-col">
-            <Input type="text" placeholder="Digite o seu email"/>
+          <form className="w-11/12 flex flex-col" onSubmit={handleLogin}>
+            <Input type="text" placeholder="Digite o seu email" value={email} onChange={ (e) => setEmail(e.target.value)}/>
 
-            <Input type="password" placeholder="Sua senha"/>
+            <Input type="password" placeholder="Sua senha" value={password} onChange={ (e) => setPassword(e.target.value)}/>
 
             <Button type="submit" loading={false}>
               Acessar
