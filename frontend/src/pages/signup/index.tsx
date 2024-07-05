01db302a4969c6797ec7b4468258e462413de0c8
gabrielglp <gabriel_pg08@hotmail.com>
2024-07-05 17:32:55 -0300

    feat: layout signup page done

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,18 +1,18 @@
 import Head from "next/head";
 
-import LogoImg from '../../public/logo.svg'
+import LogoImg from '../../../public/logo.svg'
 import Image from "next/image";
 
-import { Input } from "../components/ui/Input";
-import { Button } from "../components/ui/Button";
+import { Input } from "../../components/ui/Input";
+import { Button } from "../../components/ui/Button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>Faça o seu cadastro agora!</title>
       </Head>
       <div className="min-h-[100vh] flex justify-center items-center flex-col bg-grey-900">
         <Image
@@ -21,18 +21,22 @@ export default function Home() {
         />
 
         <div className="mt-8 w-full md:w-[600px] flex items-center justify-center flex-col py-8 px-8">
+          <h1 className="text-white pb-4 font-semibold text-3xl">Criando sua conta</h1>
+
           <form className="w-11/12 flex flex-col">
+            <Input type="text" placeholder="Digite seu nome"/>
+
             <Input type="text" placeholder="Digite o seu email"/>
 
             <Input type="password" placeholder="Sua senha"/>
 
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link href="/signup" className="mt-4">   
-            <span className="text-white cursor-pointer">Não possui uma conta? Cadastre-se</span>
+          <Link href="/" className="mt-4">   
+            <span className="text-white cursor-pointer">Já possui uma conta? Faça o login!</span>
           </Link>
         </div>
       </div>
