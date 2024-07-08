dff4e874f60bff6a2fb8b6b842398543201808da
gabrielglp <gabriel_pg08@hotmail.com>
2024-07-07 21:25:39 -0300

    api integration / put the token in cookies and redirected to the dashboard when logged in

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,15 @@
 import { createContext, ReactNode, useState } from "react";
 
+import { api } from '../services/apiClient';
+
+import { destroyCookie, setCookie, parseCookies } from 'nookies';
+import Router from "next/router";
+
 type AuthContextData = {
     user: UserProps | undefined;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>;
+    signOut: () => void;
 }
 
 type UserProps = {
@@ -23,17 +29,54 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+    try {
+        destroyCookie(undefined, '@nextauth.token')
+        Router.push('/')
+    } catch {
+        console.log('erro ao deslogar')
+    }
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<UserProps | undefined>(undefined);
     const isAuthenticated = !!user; // controle de "boolean" se estiver logado ou nao.
 
     async function signIn({ email, password }: SignInProps) {
-        console.log(email)
-        console.log(password)
+        try {
+            const response = await api.post('/session', {
+                email,
+                password
+            })
+
+            // console.log(response.data)
+
+            const { id, name, token } = response.data
+
+            setCookie(undefined, '@nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // expira em 1 mes
+                path: "/" // quais caminhos terao acesso ao cookie
+            })
+
+            setUser({
+                id,
+                name,
+                email,
+            })
+
+            // passar para proximas requisições o token
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            //Redirecionar o user para /dashbord
+            Router.push('/dashbord')
+
+        } catch(err) {
+            console.log("Error ao acessar", err)
+        }
     }
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
     );
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from '../contexts/AuthContext';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AuthProvider>
   )
 }
